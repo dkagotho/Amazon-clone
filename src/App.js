a2beb4c8827f5f0aa19e2c0ad8034032202ac824
Diana kagotho <dianakago2@gmail.com>
2020-10-24 01:27:53 -0400

    build nav bar/header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
@@ -10,11 +11,12 @@ function App() {
           <Route path="/checkout">
             <h1>checkout</h1>
           </Route>
-          <Router path="/login">
+          <Route path="/login">
             <h1>Login page</h1>
-          </Router>
+          </Route>
           {/* this is the default route */}
           <Route path="/">
+            <Header />
             <h1>Home Page</h1>
           </Route>
         </Switch>
