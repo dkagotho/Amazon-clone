04d0abcffdee995ea8db4fc4b7b643e828399b66
Diana kagotho <dianakago2@gmail.com>
2020-10-24 03:51:44 -0400

    Build banner

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/15/digital/video/merch/2020/Other/BRND_MTH20_00000_GWBleedingHero_1500x600_Final_en-CA_ENG_PVD5581._CB403140241_.jpg"
+        alt="photo"
+      />
+    </div>
+  );
+}
+
+export default Home;
