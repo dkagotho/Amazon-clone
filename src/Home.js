606342986b9f8500b2317d00f46c3cef1eb4f6a6
Diana kagotho <dianakago2@gmail.com>
2020-10-24 13:30:30 -0400

    set up properties for different products

--- src/Home.js
+++ src/Home.js
@@ -11,14 +11,65 @@ function Home() {
         alt="photo"
       />
 
-      <Product
-        id="12321341"
-        title="The Art Of War."
-        price={4.47}
-        rating={5}
-        image="https://m.media-amazon.com/images/I/41BYT1dc4QL.jpg"
-        alt="book"
-      />
+      <div className="home__row">
+        <Product
+          id="12321341"
+          title="BULZER Grid Wire Panel Board,Wall Picture Decoration for Room Clip Photo Holder Photograph Mat Hanging Art Display Frames Desk Storage Organizer"
+          price={28.04}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/71H2gU-7abL._SR149,210_.jpg"
+          alt="board"
+        />
+
+        <Product
+          id="12321341"
+          title="Oculus Rift S PC-Powered VR Gaming Headset - Windows and Controllers Accessories Travel Bag (Gray)"
+          price={549.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/51odsYyURHL._AC_UL320_.jpg"
+          alt="vr headset"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="12345678"
+          title="The Art Of War."
+          price={4.47}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/41BYT1dc4QL.jpg"
+          alt="book"
+        />
+
+        <Product
+          id="12321341"
+          title="Monitor Stand Riser with Hub, USB 3.0 Ports and Type C Port, Desktop Stand Support Fast Charging and Transfer Data"
+          price={64.99}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/71pay-L7MPL._AC_UL160_SR160,160_.jpg"
+          alt="stand"
+        />
+
+        <Product
+          id="12321341"
+          title="PreSonus Eris E4.5 2-Way Powered Studio Monitor - Eris-e4.5"
+          price={299.0}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81Hs23FJqzL._AC_UL320_.jpg"
+          alt="speakers"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="12321341"
+          title="LG 49SM8600PUA Nano 8 Series 49inches 4K Ultra HD Smart LED NanoCell TV (2019), Black (Renewed)"
+          price={679.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81ulMUKm6ZL._AC_UL320_.jpg"
+          alt="tv"
+        />
+      </div>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -4,17 +4,19 @@ import "./Product.css";
 function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
-      <p>{title}</p>
-      <p ClassName="product__price">
-        <small>CDN</small>
-        <strong>{price}</strong>
-      </p>
-      <div className="product__rating">
-        {Array(rating)
-          .fill()
-          .map((_) => (
-            <p>X</p>
-          ))}
+      <div className="product__info">
+        <p>{title}</p>
+        <p ClassName="product__price">
+          <small>CDN</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>🌟</p>
+            ))}
+        </div>
       </div>
       <img src={image} alt="book image" />
       <button>Add to basket</button>
